5cb0cae8c784df5406978266ba1c640eb19b40a5
XZANATOL <abdelrahmanbedox@gmail.com>
2025-10-16 13:12:36 +0300

    Fixing vercel deployment

--- backend/next.config.ts
+++ backend/next.config.ts
@@ -1,6 +1,8 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+	target: 'serverless',
+}
 
 export default withPayload(nextConfig)
